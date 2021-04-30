505f04b6fba02c44a5aa542b3c7e672678c09356
Andy <qingjielu990916@gmail.com>
2021-04-30 12:40:53 -0400

    alpha vectorization

--- starterCode/hmm.py
+++ starterCode/hmm.py
@@ -162,7 +162,7 @@ class HMM:
         #         alpha[t][i] *= c[t]
 
         logProb = np.log10(c).sum()
-
+        print(logProb)
         # for i in range(T):
         #     logProb += math.log10(c[i])
         logProb = -logProb
