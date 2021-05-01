46cff351f5bc6b8618883613636d4581bbd961b7
Andy <qingjielu990916@gmail.com>
2021-05-01 16:52:46 -0400

    optimize mid reestimation

--- starterCode/hmm.py
+++ starterCode/hmm.py
@@ -290,8 +290,8 @@ class HMM:
         denom = gamma1.sum(axis=0) #1*N
         numer = np.zeros((num_states, self.vocab_size))
         for t in range(T):
-            print((big_file[t] - 1))
-            numer[:,(big_file[t] - 1)] += gamma1[t].transpose()
+            # print((big_file[t] - 1))
+            numer[:,int(big_file[t] - 1)] += gamma1[t].transpose()
         emissions = numer / denom[:, None]
 
 
